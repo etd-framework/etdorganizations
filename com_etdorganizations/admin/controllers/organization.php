65f4cee4682a97891097a51b649dc6a5aa3fd4a1
Maxence Delattre <maxencedelattre62@gmail.com>
2018-03-12 14:53:46 +0100

    v1.0.1

--- com_etdorganizations/admin/controllers/organization.php
+++ com_etdorganizations/admin/controllers/organization.php
@@ -3,7 +3,7 @@
  * @package     Joomla.Administrator
  * @subpackage  com_etdorganizations
  *
- * @version     1.0.0
+ * @version     1.0.1
  * @copyright	Copyright (C) 2017 - 2018 ETD Solutions. All rights reserved.
  * @license		GNU General Public License v3
  * @author		ETD Solutions http://www.etd-solutions.com
