a8e540a0c4a411e72a37d0dc6685934035e87f5f
Maxence Delattre <maxencedelattre62@gmail.com>
2018-03-21 13:53:03 +0100

    Affichage categorie d'organisations

--- com_etdorganizations/site/views/category/tmpl/default.php
+++ com_etdorganizations/site/views/category/tmpl/default.php
@@ -21,31 +21,31 @@ if (!empty($pageclass_sfx)) {
 
 ?>
 <div class="organizations<?php echo $class; ?>">
-	<?php if ($this->params->get('show_page_heading') != 0 ) : ?>
-	<h1>
-		<?php echo $this->escape($this->params->get('page_heading')); ?>
-	</h1>
-	<?php endif; ?>
-	<div class="row">
-		<?php foreach ($this->items as $item) : ?>
-		<?php
-			$this->_item = &$item;
-			echo $this->loadTemplate('item');
-		?>
-		<?php endforeach; ?>
-	</div>
-	<?php if ($this->params->def('show_pagination', 2) == 1  || ($this->params->get('show_pagination') == 2 && $this->pagination->pagesTotal > 1)) : ?>
-	<div class="pagination-wrapper">
-		<div class="row">
-			<div class="col-md-9 col-lg-10">
-				<?php echo $this->pagination->getPagesLinks(); ?>
-			</div>
-			<?php if ($this->params->def('show_pagination_results', 1)) : ?>
-				<div class="col-md-3 col-lg-2">
-					<span class="counter"><?php echo $this->pagination->getPagesCounter(); ?></span>
-				</div>
-			<?php  endif; ?>
-		</div>
-	</div>
-	<?php endif; ?>
+    <?php if ($this->params->get('show_page_heading') != 0 ) : ?>
+        <h1>
+            <?php echo $this->escape($this->params->get('page_heading')); ?>
+        </h1>
+    <?php endif; ?>
+
+    <?php foreach ($this->items as $item) : ?>
+        <?php
+        $this->_item = &$item;
+        echo $this->loadTemplate('item');
+        ?>
+    <?php endforeach; ?>
+
+    <?php if ($this->params->def('show_pagination', 2) == 1  || ($this->params->get('show_pagination') == 2 && $this->pagination->pagesTotal > 1)) : ?>
+        <div class="pagination-wrapper">
+            <div class="row">
+                <div class="col-md-9 col-lg-10">
+                    <?php echo $this->pagination->getPagesLinks(); ?>
+                </div>
+                <?php if ($this->params->def('show_pagination_results', 1)) : ?>
+                    <div class="col-md-3 col-lg-2">
+                        <span class="counter"><?php echo $this->pagination->getPagesCounter(); ?></span>
+                    </div>
+                <?php  endif; ?>
+            </div>
+        </div>
+    <?php endif; ?>
 </div>
